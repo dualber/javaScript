9d7fedc27a9e2c374dd6e9db85467f11944a4ac6
dualbert <dualber29@gmail.com>
2025-12-01 22:39:24 -0500

    solucion a la clase persona

--- Poo/crud/principal.js
+++ Poo/crud/principal.js
@@ -1,5 +1,11 @@
-import {Persona} from persona.js
+import {Persona} from './persona.js';
 
-const persona1 = new Persona('dualber',24, 1.70);
+const persona1 = new Persona('dualber',24,1.70);
+let nombrePersona = document.getElementById('nombre');
+let edadPersona = document.getElementById('edad');
+let alturaPersona = document.getElementById('altura');
 
-console.log(persona1);
+nombrePersona.innerText = persona1.nombre;
+edadPersona.innerText = persona1.Edad;
+alturaPersona.innerText = persona1.altura;
+
